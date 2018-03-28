ecbac792e54a2ea49792043438600819e8e325eb
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-28 16:12:28 +0300

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,1 +1,1 @@
-echo $(date -u) "Build: ${BUILD_NUMBER} / Branch: $BRANCH_NAME / Processing provided DSL Script / Finished: SUCCESS / \n"
+echo $(date -u) "Build: ${BUILD_NUMBER} / Branch: $BRANCH_NAME / Processing provided DSL Script / Finished: SUCCESS "
